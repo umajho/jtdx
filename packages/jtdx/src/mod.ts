ced632cdce5be655514f08ce64148c582d197fa0
umajho <umajho@proton.me>
2024-09-28 18:36:18 +0800

    feat: export some types

--- packages/jtdx/src/mod.ts
+++ packages/jtdx/src/mod.ts
@@ -1,1 +1,15 @@
+export type { RootSchema } from "./types";
+export type {
+  CompilationOptions,
+  CompilationResult,
+  ValidationResult,
+  Validator,
+} from "./compiling";
+export type {
+  CompilationError,
+  CompilationRawError,
+  ValidationError,
+  ValidationRawError,
+} from "./errors";
+
 export { compile } from "./compiling";
